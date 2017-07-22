ccd7ba9bfc471110e4f1c07a8a70d897adbd93a8
javiervidal15 <javier_07_95@hotmail.com>
2017-07-22 05:03:46 -0300

    Form contacto funcionando, falta arreglar detalles

--- contact.php
+++ contact.php
@@ -1,64 +1,33 @@
 <?php
-// PHP extensions Mail and Net_SMTP must be installed. If they are not already
-// installed, install them by PEAR:
-// > pear install Mail
-// > pear install Net_SMTP
-// SSL must be allowed. On Windows you should uncomment line in php.ini:
-//		extension=php_openssl.dll
-// If you use smtp.gmail.com as smpt host then use:
-//		https://accounts.google.com/DisplayUnlockCaptcha
-// and allow access for less secure apps:
-//		https://www.google.com/settings/security/lesssecureapps
-//		
-// =============================================================================
-// NOTES
-// contact.php – is a script for a web servers with php support.
-// Extensions which required for this script are often installed by default.
-// If they are not already installed, the easiest way to do this using PEAR.
-// Learn more about PEAR http://pear.php.net/.
-// Editing the php.ini required only on Windows. On other operating systems this
-// is usually not required. Location of this file depends on settings of web
-// server.
-// Also, your web server should not be denied access to external mail servers.
-// On the majority of paid and on many free hostings (eg OpenShift:
-// https://www.openshift.com/) all these features are available. 
-// =============================================================================
 
-require_once "Mail.php"; 
-// Change this options:
-$username = 'agenciastucia@gmail.com';
-$password = 'Inboxq1w2e3r4t5*';
-$smtpHost = 'ssl://smtp.gmail.com';
-$smtpPort = '465';
-$to = 'agenciastucia@gmail.com';
-$from = 'agenciastucia@gmail.com';
+require './mailer/class.phpmailer.php';
+require './mailer/class.smtp.php';
 
-$subject = 'Contact Form';
-$successMessage = 'Message successfully sent!';
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->SMTPDebug  = 0;
 
-$replyTo = $_POST['your-email'];
-$name = $_POST['your-name'];
-$body = $_POST['your-message'];
+$mail->Host       = 'smtp.gmail.com';
+$mail->Port       = 587;
+$mail->SMTPSecure = 'tls';
+$mail->SMTPAuth   = true;
+$mail->Username   = "agenciastucia@gmail.com";
+$mail->Password   = "Inboxq1w2e3r4t5*";
 
-$headers = array(
-	'From' => $name . " <" . $from . ">",
-	'Reply-To' => $name . " <" . $replyTo . ">",
-	'To' => $to,
-	'Subject' => $subject
-);
-$smtp = Mail::factory('smtp', array(
-			'host' => $smtpHost,
-			'port' => $smtpPort,
-			'auth' => true,
-			'username' => $username,
-			'password' => $password
-		));
+//Quien envia el mensaje (mail,nombre)
+$mail->SetFrom('agenciastucia@gmail.com', 'Javier');
+//Quien recibe el mensaje (mail,nombre)
+$mail->AddAddress('agenciastucia@gmail.com', 'El Destinatario');
+//Asunto
+$mail->Subject = 'Esto es un correo de prueba';
+//Mensaje en formato HTML
+$mail->MsgHTML("Hola");
+//Mensaje en formato texto plano por si se rechaza el HTML
+$mail->AltBody = 'This is a plain-text message body';
 
-$mail = $smtp->send($to, $headers, $body);
-
-if (PEAR::isError($mail)) {
-	echo($mail->getMessage());
+//Envio de mail
+if(!$mail->Send()) {
+  echo "Error: " . $mail->ErrorInfo;
 } else {
-	echo($successMessage);
+  echo "Enviado!";
 }
-?>
